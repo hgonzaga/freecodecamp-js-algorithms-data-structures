afc898006b157e0b6dead63cfb23ad97598b5040
Hugo Gonzaga <hugonzagamigl@gmail.com>
2021-12-12 21:45:04 +0000

    Basic JavaScript (ex. 51 to 53)

---
+++ 1-basic-javascript/051-return-value-from-function-wit-return.js
@@ -0,0 +1,12 @@
+/*
+Return a Value from a Function with Return:
+Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+- timesFive should be a function
+- timesFive(5) should return 25
+- timesFive(2) should return 10
+- timesFive(0) should return 0
+ */
+function timesFive(num) {
+    return num * 5;
+}
---
+++ 1-basic-javascript/053-local-scope-and-functions.js
@@ -0,0 +1,20 @@
+/*
+Local Scope and Functions:
+The editor has two console.logs to help you see what is happening.
+Check the console as you code to see how it changes. Declare a local variable myVar inside myLocalScope and run the tests.
+
+Note: The console will still display ReferenceError: myVar is not defined, but this will not cause the tests to fail.
+
+- The code should not contain a global myVar variable.
+- You should add a local myVar variable.
+ */
+function myLocalScope() {
+    // Only changed code below this line
+    let myVar = 1;
+    console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
