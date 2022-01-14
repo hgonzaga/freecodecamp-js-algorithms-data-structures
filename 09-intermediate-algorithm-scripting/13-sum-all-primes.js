7a1b86a0ad8d3f8ee3c22276a7360e0d4dc6baf8
Hugo Gonzaga <hugonzagamigl@gmail.com>
2022-01-14 00:20:06 +0000

    Intermediate Algorithm Scripting (ex. 13)

--- 09-intermediate-algorithm-scripting/13-sum-all-primes.js
+++ 09-intermediate-algorithm-scripting/13-sum-all-primes.js
@@ -10,3 +10,24 @@ Rewrite sumPrimes so it returns the sum of all prime numbers that are less than 
 - sumPrimes(10) should return 17.
 - sumPrimes(977) should return 73156.
 */
+function sumPrimes(num) {
+    let sum = 0;
+    for (let i = 2; i <= num; i++) {
+        if (isPrime(i)) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+function isPrime(num) {
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+    return num > 1;
+}
+
+console.log(sumPrimes(10));
+console.log(sumPrimes(977));
